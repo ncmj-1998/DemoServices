edd23e5c2193301ef9e4d05a19d5db9612740a94
16022671 <16022671@myrp.edu.sg>
2018-07-17 16:32:20 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/demoservices/MainActivity.java
@@ -0,0 +1,36 @@
+package sg.edu.rp.webservices.demoservices;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.start);
+        Button btnStop = findViewById(R.id.stop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                startService(i);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                stopService(i);
+            }
+        });
+
+    }
+}
